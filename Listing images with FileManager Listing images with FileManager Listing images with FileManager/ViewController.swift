4fccb5cfa7ca93856f653d4ec17776ebfd23c7bb
Alan Casas <alancasasarevalo@gmail.com>
2018-09-02 09:12:06 +0200

    Initial Commit

---
+++ Listing images with FileManager Listing images with FileManager Listing images with FileManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listing images with FileManager Listing images with FileManager Listing images with FileManager
+//
+//  Created by Alan Casas on 2/9/18.
+//  Copyright © 2018 Alan Casas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
